485f7847b2c7af3e62bf14cab8a27e3461031f54
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2021-01-21 12:29:19 +0300

    p102+ update

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 exports.ClientMod = class {
-   install(installer) {
+  constructor(mod) {
+    if(mod.majorPatchVersion > 101) {
+      mod.log("unsupported patch version")
+      return;
+    }
+  }
+  
+  install(installer) {
      installer.gpk("S1UI_GameModeSelectScene.gpk");
-   }
+  }
 };
